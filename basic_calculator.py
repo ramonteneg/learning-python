1eee6b821d4fb21ef9dff44e4ac7fd73f5247054
ramonteneg <156733500+ramonteneg@users.noreply.github.com>
2024-01-28 16:50:07 -0500

    Create basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,23 @@
+num1 = 20
+num2 = 3
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(" BASIC CALC V1.0 ")
+print("Number 1: ", num1, type(num1))
+print("Number 2: ", num2, type(num2))
+print("The addition is: ", add, type(add))
+print("The subtraction is: ", sub, type(sub))
+print("The multiplication is: ", mul, type(mul))
+print("The division is: ", div, type(div))
+print("\n")
+print("BASIC CALC V1.1")
+print("Number 1: ", num1)
+print("Number 2: ", num2)
+print("The addition is: ", num1+num2)
+print("The subtraction is: ", num1-num2)
+print("The multiplication is: ", num1*num2)
+print("The division is: ", num1/num2)
